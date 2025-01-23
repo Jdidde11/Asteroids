64a7fed998b4c9adf79a9ccb0eb465e6a65cb708
DiddlyDoHome <diddyneedssomepity@gmail.com>
2025-01-24 00:08:13 +0900

    Changes to be committed: 	modified:   __pycache__/constants.cpython-312.pyc 	modified:   constants.py 	modified:   main.py 	new file:   player.py

--- main.py
+++ main.py
@@ -1,22 +1,29 @@
 import pygame
 from constants import *
+from player import Player
 
-
+#Activate the virtual environment
+#source venv/bin/activate
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     dt = 0
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+            
+        player.update(dt)
 
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
 
         # limit the framerate to 60 FPS
+        # dt is Delta Time
         dt = clock.tick(60) / 1000
 
 
