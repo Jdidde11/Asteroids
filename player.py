ad827fbb10f80d37262003f6dbf4e7fcfc835f03
DiddlyDoHome <diddyneedssomepity@gmail.com>
2025-02-02 23:39:59 +0900

    Changes to be committed: 	modified:   __pycache__/constants.cpython-312.pyc 	new file:   asteroid.py 	new file:   asteroidfield.py 	modified:   circleshape.py 	modified:   constants.py 	modified:   main.py 	modified:   player.py 	new file:   shot.py

--- player.py
+++ player.py
@@ -1,16 +1,17 @@
 import pygame
 from constants import *
 from circleshape import CircleShape
+from shot import Shot
+
 
 class Player(CircleShape):
     def __init__(self, x, y):
         super().__init__(x, y, PLAYER_RADIUS)
         self.rotation = 0
-        
+
     def draw(self, screen):
         pygame.draw.polygon(screen, "white", self.triangle(), 2)
 
-        # in the player class
     def triangle(self):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
         right = pygame.Vector2(0, 1).rotate(self.rotation + 90) * self.radius / 1.5
@@ -18,7 +19,6 @@ class Player(CircleShape):
         b = self.position - forward * self.radius - right
         c = self.position - forward * self.radius + right
         return [a, b, c]
-
 
     def update(self, dt):
         keys = pygame.key.get_pressed()
@@ -31,6 +31,12 @@ class Player(CircleShape):
             self.rotate(-dt)
         if keys[pygame.K_d]:
             self.rotate(dt)
+        if keys[pygame.K_SPACE]:
+            self.shoot()
+
+    def shoot(self):
+        shot = Shot(self.position.x, self.position.y)
+        shot.velocity = pygame.Vector2(0, 1).rotate(self.rotation) * PLAYER_SHOOT_SPEED
 
     def rotate(self, dt):
         self.rotation += PLAYER_TURN_SPEED * dt
